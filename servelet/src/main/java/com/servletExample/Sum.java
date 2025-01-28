2847133bbf942e83ae1abdeede0e2488eee65236
ashraftuyubahe <ashrafutyubahe001u@gmail.com>
2025-01-28 07:32:48 +0200

    initialised project

---
+++ servelet/src/main/java/com/servletExample/Sum.java
@@ -0,0 +1,21 @@
+package com.servletExample;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Sum extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        int num1 = Integer.parseInt(req.getParameter("num1"));
+        int num2 = Integer.parseInt(req.getParameter("num2"));
+        int sum = num1 + num2;
+
+        resp.setContentType("text/html");
+        resp.getWriter().println("<h1>Sum is: " + sum + "</h1>");
+    }
+
+}
---
+++ servelet/target/servelet/index.jsp
@@ -0,0 +1,11 @@
+<html>
+<body>
+    <form action="sum" method="post">
+        <label>Enter Number 1:</label>
+        <input type="text" name="num1" />
+        <label>Enter Number 2:</label>
+        <input type="text" name="num2" />
+        <button type="submit">Calculate</button>
+    </form>
+</body>
+</html>
