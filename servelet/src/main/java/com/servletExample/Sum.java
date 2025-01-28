cc5c9a58cbf0698141d3ac6f83f54d6c304bef9e
ashraftuyubahe <ashrafutyubahe001u@gmail.com>
2025-01-28 11:02:43 +0200

    using session to add the data within the request

--- servelet/src/main/java/com/servletExample/Sum.java
+++ servelet/src/main/java/com/servletExample/Sum.java
@@ -1,28 +1,28 @@
 package com.servletExample;
 
 import java.io.IOException;
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 public class Sum extends HttpServlet {
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         try {
-
             int num1 = Integer.parseInt(req.getParameter("num1"));
             int num2 = Integer.parseInt(req.getParameter("num2"));
             int sum = num1 + num2;
 
-            req.setAttribute("total", sum);
-
-            resp.setContentType("text/html");
-            resp.getWriter().println("<h1>Sum is: " + sum + "</h1>");
-
-            RequestDispatcher rd = req.getRequestDispatcher("/total");
-            rd.forward(req, resp);
+            // req.setAttribute("total", sum);
+         
+            // resp.setContentType("text/html");
+            // resp.getWriter().println("<h1>Sum is: " + sum + "</h1>");
+            
+         HttpSession  session= req.getSession();
+         session.setAttribute("total", sum);
+            resp.sendRedirect("total?sum=" + sum); 
         } catch (Exception e) {
             resp.getWriter().println("<h1>Error: Invalid input or calculation.</h1>");
         }
--- servelet/src/main/java/com/servletExample/TotalServlet.java
+++ servelet/src/main/java/com/servletExample/TotalServlet.java
@@ -1,19 +1,30 @@
 package com.servletExample;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import java.io.PrintWriter;
 
 public class TotalServlet extends HttpServlet {
 
     @Override
     public void service(HttpServletRequest req, HttpServletResponse res) throws IOException {
+        
+        // String sumParam = req.getParameter("sum");
+        HttpSession sumParam= req.getSession();
+        int total= (int)sumParam.getAttribute("total");
+        // Integer total = null;
 
-        Integer total = (Integer) req.getAttribute("total");
-
+       
         PrintWriter out = res.getWriter();
-        out.print("<h1>The total is: " + total + "</h1>");
+        out.print("<h1> after redirect ,using session The total is: " + total + "</h1>");
+        // if (total != null) {
+        //     out.print("<h1> after redirect ,The total is: " + total + "</h1>");
+        // } else {
+        //     out.print("<h1>Error: Unable to retrieve the total.</h1>");
+        // }
     }
 }
